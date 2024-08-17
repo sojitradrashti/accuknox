d28879870478b937c9e269c285af66dd9ef82152
sojitradrashti <sojitradrashti5@gmail.com>
2024-08-16 22:48:44 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Dashboard from './component/Dashboard';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,48 +1,113 @@
 import React from "react";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import { Box, InputAdornment, Link, TextField, Typography } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import {
+  Box,
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import { PieChart } from '@mui/x-charts/PieChart';
+import { useDrawingArea } from '@mui/x-charts/hooks';
+import { styled } from '@mui/material/styles';
 const Dashboard = () => {
-  const breadcrumbs = [
-    <Link underline="none" key={2} color="inherit">
-      Home
-    </Link>,
+  const names = ["Last 2 days", "Last week", "Last month", "Last year"];
 
-    <Typography key="3" color="blue">
-      Dashboard V2
-    </Typography>,
+  const data = [
+    { value: 5, label: 'A' },
+    { value: 10, label: 'B' },
+    { value: 15, label: 'C' },
+    { value: 20, label: 'D' },
   ];
 
+  const size = {
+    width: 400,
+    height: 200,
+  };
+
+  const StyledText = styled('text')(({ theme }) => ({
+    fill: theme.palette.text.primary,
+    textAnchor: 'middle',
+    dominantBaseline: 'central',
+    fontSize: 20,
+  }));
+
+  
+  function PieCenterLabel({ children }) {
+    const { width, height, left, top } = useDrawingArea();
+    return (
+      <StyledText x={left + width / 2} y={top + height / 2}>
+        {children}
+      </StyledText>
+    );
+  }
   return (
-    <Box display="flex" alignItems="center" justifyContent="space-between" paddingX="5px">
+    <Box
+      backgroundColor="#F0F5FA"
+      borderRadius="10px"
+      padding="5px"
+      marginTop="25px"
+    >
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        paddingX="25px"
+        marginTop="35px"
+      >
         <Box>
-        <Breadcrumbs separator="›" aria-label="breadcrumb">
-          {breadcrumbs}
-        </Breadcrumbs>
+          <Typography>CNAPP Dashboard</Typography>
         </Box>
+        <Box>
+          <Button variant="outlined" sx={{ color: "primary" }}>
+            Add Widget
+            <AddIcon />
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{ color: "primary", marginLeft: "15px" }}
+          >
+            <AutorenewIcon />
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{ color: "primary", marginLeft: "15px" }}
+          >
+            <MoreVertIcon />
+          </Button>
 
-        <Box display="flex" marginTop="4px" alignItems="center" gap="20px">
-        <TextField
-        sx={{width:"80vh"}}
-          size="small"
-          autoComplete="off"
-          placeholder="Search anything..."
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-
-        <NotificationsActiveIcon />
-        <AccountCircleIcon />
+          <FormControl
+            sx={{ m: 1, minWidth: 120, position: "relative", marginTop: "0px" }}
+          >
+            <AccessTimeFilledIcon sx={{ position: "absolute", my: 1, mx: 1 }} />
+            <Select
+              sx={{ alignItems: "center", height: "40px" }}
+              size="small"
+              labelId="demo-controlled-open-select-label"
+              id="demo-controlled-open-select"
+            >
+              {names.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Box>
-
       </Box>
+
+      <Box paddingX="25px">
+        <Typography align="left">CSPM Executive Dashboard</Typography>
+      </Box>
+      <Box>
+        <PieChart series={[{ data, innerRadius: 80 }]} {...size}>
+          <PieCenterLabel>Center label</PieCenterLabel>
+        </PieChart>
+      </Box>
+    </Box>
   );
 };
 
